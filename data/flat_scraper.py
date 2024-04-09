5f636f1b9fa6c2444d9c54bc155620d18a2e870c
WaznyKamo <waznykamil@wp.pl>
2024-04-09 21:35:44 +0200

    w sumie niewiele sie zmienilo

--- data/flat_scraper.py
+++ data/flat_scraper.py
@@ -12,15 +12,19 @@ import re
 
 
 file_path = r"C:\Users\Kamil\OneDrive\Inżynieria danych i Data Science\Tricity-housing\data\flat-sell.csv"
-url_site = "https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/pomorskie/gdansk/gdansk/gdansk?ownerTypeSingleSelect=ALL&by=DEFAULT&direction=DESC&viewType=listing&page="
+url_site = "https://www.morizon.pl/mieszkania/gdansk/?ps%5Blocation%5D%5Bmap%5D=1&ps%5Blocation%5D%5Bmap_bounds%5D=54.4472188%2C18.9512795%3A54.2749559%2C18.4287748&ps%5Bwith_price%5D=1&page="
 
 
 data = pd.DataFrame()
-response = requests.get("https://www.morizon.pl/mieszkania/gdansk/")
+response = requests.get(url_site + '1')
 soup = BeautifulSoup(response.text, features='html.parser')
     
 pages = int(soup.find_all("div", attrs={"class": "I03LY0"})[-2].text)
     
 
 for i in range(1, pages+1):
-    print(i)
+    url = url_site + str(i)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, features='html.parser')
+    
+    flat_list = soup.find('div', attrs={"class": "ooa-r53y0q ezh3mkl11"})
